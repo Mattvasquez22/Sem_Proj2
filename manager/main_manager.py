5b55118b0ed0fba038de6cf1b07182ca6aade2ea
Mateo Vasquez <leonel.vasquez@eurecom.fr>
2019-06-25 07:47:08 +0200

    Deleted unnecessary comments
    Commit before presentation

--- manager/main_manager.py
+++ manager/main_manager.py
@@ -19,25 +19,23 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 if __name__=="__main__":
-    parser =  ConfigParser.ConfigParser()
-    parser.read('config.ini')
-    HOST = parser.get('Settings', 'host_manager')
-    PORT = int(parser.get('Settings', 'port_manager'))
-    MAX_CLIENTS = int(parser.get('Settings', 'max_clients'))
-    CHECK_TIME = int(parser.get('Settings', 'check_time'))
-    server = SocketServer(HOST,PORT,MAX_CLIENTS)
-    server.daemon = True
-    server.start()
-    #Neded to check every x time for the timestamps of the clients in the pool
-    checker = task.LoopingCall(checkTimestamp)
-    checker.start(CHECK_TIME)
-    reactor.run()
+try:
+        parser =  ConfigParser.ConfigParser()
+        parser.read('config.ini')
+        HOST = parser.get('Settings', 'host_manager')
+        PORT = int(parser.get('Settings', 'port_manager'))
+        MAX_CLIENTS = int(parser.get('Settings', 'max_clients'))
+        CHECK_TIME = int(parser.get('Settings', 'check_time'))
+        server = SocketServer(HOST,PORT,MAX_CLIENTS)
+        server.daemon = True
+        server.start()
+        #Neded to check every x time for the timestamps of the clients in the pool
+        checker = task.LoopingCall(checkTimestamp)
+        checker.start(CHECK_TIME)
+        reactor.run()
 
-    signal.signal(signal.SIGINT, signal_handler)
-    while True:
-        #q = raw_input("Check for pool: ")
-        #if(q == 'Y'):
-        #   print(server.sock_pool)
-        #   print(server.sock_timestamp)
-        pass
-
+        signal.signal(signal.SIGINT, signal_handler)
+        while True:
+            pass
+except:
+    pass
